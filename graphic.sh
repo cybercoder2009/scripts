6cae3dc864545ce28081eff9196cd5e4afbfab74
chris <chris@aion.network>
2020-02-24 00:19:16 +0800

    update

--- graphic.sh
+++ graphic.sh
@@ -1,1 +1,13 @@
+# check graphic cards
 lshw -numeric -C display
+
+# disable nouveau
+sudo nano /etc/modprobe.d/blacklist.conf
+blacklist vga16fb
+blacklist nouveau
+blacklist rivafb
+blacklist rivatv
+blacklist nvidiafb
+
+
+
